6c66cfeed3f100c78f3a047a5199d2d75f6170a1
daniel20max <51431192+daniel20max@users.noreply.github.com>
2021-11-05 21:13:17 -0300

    faltando CSS

--- index.js
+++ index.js
@@ -12,34 +12,33 @@ const pokedex = [];
 let message = "";
 
 app.get("/", (req, res) => {
-    setTimeout(() => {
-        message = "";
-      }, 5000);
-
+  setTimeout(() => {
+    message = "";
+    }, 5000);
     
-    res.render("index", {
-        pokedex,
-        message,
-    });
+  res.render("index", {
+    pokedex,
+    message,
   });
+});
 
-  app.get("/Cadastro", (req, res) =>{
-      res.render("Cadastro")
-  })
+app.get("/Cadastro", (req, res) =>{
+  res.render("Cadastro")
+})
 
-  app.post("/Add", (req, res) => {
-    const pokemon = req.body;
-    pokedex.push(pokemon);
-    message = 'O Pokemon '+ pokemon.nome +' foi adicionado a Pokedex';
-    res.redirect("/");
+app.post("/Add", (req, res) => {
+  const pokemon = req.body;
+  pokedex.push(pokemon);
+  message = 'O Pokemon '+ pokemon.nome +' foi adicionado a Pokedex';
+  res.redirect("/");
+});
+
+app.get("/Deck/:id", (req, res) => {
+  const id = req.params.id;
+  const pokemon = pokedex[id];
+  res.render("Deck", {
+    pokemon,
   });
-
-// app.get("/Deck/:id", (req, res) => {
-//   const id = req.params.id;
-//   const pokemon = pokedex[id]
-//   res.render("Deck", {
-//     pokemon,
-//   });
-// })
+});
 
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
